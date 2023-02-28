2b0c349e9ec94f0ba233f6cfd14f6d8277b2a777
SoderstromJohnR <56499654+SoderstromJohnR@users.noreply.github.com>
2023-02-27 22:32:42 -0600

    First round of tutorial changes and refreshers on material, including 2-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
