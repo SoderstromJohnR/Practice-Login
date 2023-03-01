59d71702da59e7df990485bc703b851e0c75d483
SoderstromJohnR <56499654+SoderstromJohnR@users.noreply.github.com>
2023-02-28 22:02:53 -0600

    Started to include router, applying tailwind CSS as per tutorial. Services added, book component fleshed out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
-import { BookComponent } from './book/book.component';
+import { BooksModule } from './books/books.module';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BookComponent
+    CartComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BooksModule
   ],
-  providers: [],
+  providers: [BooksModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from '../types/Book';
+import { BooksService } from './books.service';
 
 
 
@@ -9,30 +10,14 @@ import { Book } from '../types/Book';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
-  books: Book[] = [
-    {
-      name: 'Clean code',
-      author: 'gert T Yore',
-      image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
-      amount: 24,
-    },
-    {
-      name: 'pragmatic Programmer',
-      author: 'David thomas',
-      image: 'https://m.media-amazon.com/images/I/41HXiIojloL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-      amount: 16,
-    },
-  ];
-
+  books: Book[] = [];
   cart: Book[] = [];
 
   isShowing: boolean = true;
 
-  constructor() {}
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.getBooks();
+  }
 
   ngOnInit(): void {}
-
-  addToCart(event: Book) {
-    console.log(event.name);
-  }
 }
